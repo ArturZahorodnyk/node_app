ab1f021e810e7c1f2429d478acd138fb2c77d8ba
ArturZahorodnyk <zagorodnykartur@gmail.com>
2022-09-28 15:28:18 +0300

    Added building porject

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-cp -r /home/ec2-user/spring-boot-mongo/node_app/dist/* /usr/share/nginx/html
+npm run build /home/ec2-user/node_app/
+
+cp -r /home/ec2-user/node_app/dist/* /usr/share/nginx/html
 
 sudo systemctl start nginx
