b39671f9a351441ab417c1e7f17b4b3f2a166816
ArturZahorodnyk <zagorodnykartur@gmail.com>
2022-10-02 12:50:12 +0300

    add start setup

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,7 +6,6 @@
 #sudo cp -r /home/ec2-user/node_app/dist/* /usr/share/nginx/html
 #sudo systemctl start nginx
 
-#!/bin/bash
 cd /home/ec2-user/node_app
 docker build -t node_app/dockerize-vuejs-app .
 docker run -it -p 8080:80 --rm --name dockerize-vuejs-app-1 node_app/dockerize-vuejs-app
