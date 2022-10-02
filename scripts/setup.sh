5a88362b1fb55117e6492f2ce0d4b33b1e0b7047
ArturZahorodnyk <zagorodnykartur@gmail.com>
2022-10-02 12:43:09 +0300

    Added docker image to deploy on prod

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-sudo yum -y install epel-release
-sudo yum -y update
-sudo yum -y install curl
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
-sudo yum install -y nodejs
-sudo yum -y install nginx
+#sudo yum -y install epel-release
+#sudo yum -y update
+#sudo yum -y install curl
+#curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
+#sudo yum install -y nodejs
+#sudo yum -y install nginx
+
+sudo amazon-linux-extras install docker -y
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+sudo chmod 666 /var/run/docker.sock
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo service docker start
 
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-sudo npm i sass --save-dev
+#sudo npm i sass --save-dev
+#sudo npm install /home/ec2-user/node_app/
+#sudo npm run build /home/ec2-user/node_app/
+#sudo cp -r /home/ec2-user/node_app/dist/* /usr/share/nginx/html
+#sudo systemctl start nginx
 
-sudo npm install /home/ec2-user/node_app/
-
-sudo npm run build /home/ec2-user/node_app/
-
-sudo cp -r /home/ec2-user/node_app/dist/* /usr/share/nginx/html
-
-sudo systemctl start nginx
+#!/bin/bash
+cd /home/ec2-user/node_app
+docker build -t node_app/dockerize-vuejs-app .
+docker run -it -p 8080:80 --rm --name dockerize-vuejs-app-1 node_app/dockerize-vuejs-app
